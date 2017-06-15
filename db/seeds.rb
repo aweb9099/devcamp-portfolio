962808dc8d4b35a564fb6a612b76b664833064e9
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 16:54:08 +0000

    this is new

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,28 @@
   Blog.create!(
     title: "My Blogs #{blog}",
     body: "hello world. hi"
-    )
+  )
   end
 
 puts "10 blog posts created"
 
 5.times do |skill|
-  skill.create!(
+  Skill.create!(
     title: "Rails #{skill}",
     percent_utilized: 15
-    )
+  )
   end
   
-  puts "5 skills created"
+puts "5 skills created"
   
 9.times do |portfolio_item|
-  portfolio.create!(
+  Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
     body: "Et Tu Brute",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
-    )
+  )
   end
   
-  puts "9 portfolio items created"
+puts "9 portfolio items created"
