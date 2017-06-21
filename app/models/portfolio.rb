fc2d71652a75e7bf53bb0e807c3837a199e1ec81
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 14:55:47 +0000

    ||- things and defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
   
   scope :ruby_on_rails_portflio_items, -> { where(subtitle: 'Ruby on Rails') }
+  
+  after_initalize :set_defaults
+  
+  def set_defaults
+      self.main_image ||="http://via.placeholder.com/600x400"
+      self.thumb_image ||="http://via.placeholder.com/350x200"
+  end
 end
