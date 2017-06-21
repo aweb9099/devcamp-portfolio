6206446a20532c7f809fdd1a09a6a7a6c2f49563
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:31:27 +0000

    intigrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
   include Placeholder
   
   validates_presence_of :title, :body, :main_image, :thumb_image
@@ -9,7 +10,7 @@ class Portfolio < ApplicationRecord
   
   scope :ruby_on_rails_portflio_items, -> { where(subtitle: 'Ruby on Rails') }
   
-  after_initalize :set_defaults
+  after_initialize :set_defaults
   
   def set_defaults
       self.main_image ||=Placeholder.image_generator(height: '600', width: '400')
