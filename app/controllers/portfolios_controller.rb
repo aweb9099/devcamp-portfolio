c15127796d1773f90823b1f364be2b73b7832920
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 17:17:05 +0000

    made form config for nests

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,10 +9,11 @@ class PortfoliosController < ApplicationController
   
   def new
     @portfolio_item = portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
   
   def create
-   @portfolio_item = portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+   @portfolio_item = portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
